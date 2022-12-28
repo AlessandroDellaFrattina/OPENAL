a22c0b06a2cacb8ddef9c31dc6fc3736d252e373
Alilux <83576617+AlessandroDellaFrattina@users.noreply.github.com>
2022-12-28 19:14:31 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "OPENAL"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "On"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	includedirs {
+
+		"include",
+    "common",
+    "core",
+    "router",
+    "utils"
+	}
+
+  files {
+
+    "**.c",
+    "**.cpp"
+  }
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		defines "VULTURE_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VULTURE_RELEASE"
+		runtime "Debug"
+		symbols "On"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VULTURE_DIST"
+		runtime "Release"
+		optimize "On"
