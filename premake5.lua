c756a1ccefe598d985ab5ea640d83e9fb5fbed87
Alessandro della Frattina <alessandrodellafrattina@gmail.com>
2022-12-31 21:55:30 +0100

    Updated OPENAL submodules (not ready for build)

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
-project "OPENAL"
+project "COMMON"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
+	cppdialect "C++14"
 	staticruntime "On"
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
@@ -9,18 +9,11 @@ project "OPENAL"
 
 	includedirs {
 
+		"%{prj.location}",
 		"include",
-    		"common",
-    		"core",
-    		"router",
-    		"utils"
+		"include/AL",
+		"common"
 	}
-
-  	files {
-
-    		"**.c",
-    		"**.cpp"
-  	}
 
 	filter "system:windows"
 		systemversion "latest"
@@ -28,7 +21,223 @@ project "OPENAL"
 		defines {
 
 			"VULTURE_PLATFORM_WINDOWS",
+			"WIN32",
+			"_WINDOWS",
+			"_WIN32",
+			"NOMINMAX",
+			"RESTRICT=__restrict",
 			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+		disablewarnings {
+
+			"4065",
+			"4127",
+			"4268",
+			"4324",
+			"5030",
+			"5051"
+		}
+
+		files {
+
+			"config.h",
+			"version.h",
+			"hrtf_default.h",
+
+			"common/alcomplex.cpp",
+			"common/alfstream.cpp",
+			"common/almalloc.cpp",
+			"common/alstring.cpp",
+			"common/dynload.cpp",
+			"common/polyphase_resampler.cpp",
+			"common/ringbuffer.cpp",
+			"common/strutils.cpp",
+			"common/threads.cpp"
+		}
+
+	filter "system:linux"
+		defines "VULTURE_PLATFORM_LINUX"
+
+	filter "system:macosx"
+		defines "VULTURE_PLATFORM_MACOSX"
+
+	filter "configurations:Debug"
+		defines "VULTURE_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VULTURE_RELEASE"
+		runtime "Debug"
+		symbols "On"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VULTURE_DIST"
+		runtime "Release"
+		optimize "On"
+
+
+project "OPENAL"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++14"
+	staticruntime "On"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	includedirs {
+
+		"%{prj.location}",
+		"include",
+		"include/AL",
+		"common"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines {
+
+			"VULTURE_PLATFORM_WINDOWS",
+			"WIN32",
+			"_WINDOWS",
+			"AL_BUILD_LIBRARY",
+			"AL_ALEXT_PROTOTYPES",
+			"ALC_API=__declspec(dllexport)",
+			"AL_API=__declspec(dllexport)",
+			"_WIN32",
+			"NOMINMAX",
+			"RESTRICT=__restrict",
+			"_CRT_SECURE_NO_WARNINGS",
+			"OpenAL_EXPORTS"
+		}
+
+		disablewarnings {
+
+			"4065",
+			"4127",
+			"4268",
+			"4324",
+			"5030",
+			"5051"
+		}
+
+		links {
+
+			"winmm.lib",
+			"kernel32.lib",
+			"user32.lib",
+			"gdi32.lib",
+			"winspool.lib",
+			"shell32.lib",
+			"ole32.lib",
+			"oleaut32.lib",
+			"uuid.lib",
+			"comdlg32.lib",
+			"advapi32.lib",
+
+			"COMMON"
+		}
+
+		files {
+
+			"config.h",
+			"version.h",
+			"hrtf_default.h",
+
+			"alc/alc.cpp",
+			"alc/alconfig.cpp",
+			"alc/alu.cpp",
+			"core/ambdec.cpp",
+			"core/ambidefs.cpp",
+			"al/eax/api.cpp",
+			"al/effects/autowah.cpp",
+			"alc/effects/autowah.cpp",
+			"al/auxeffectslot.cpp",
+			"alc/backends/base.cpp",
+			"core/bformatdec.cpp",
+			"core/filters/biquad.cpp",
+			"core/bs2b.cpp",
+			"core/bsinc_tables.cpp",
+			"al/buffer.cpp",
+			"core/buffer_storage.cpp",
+			"al/eax/call.cpp",
+			"alc/effects/chorus.cpp",
+			"al/effects/chorus.cpp",
+			"al/effects/compressor.cpp",
+			"alc/effects/compressor.cpp",
+			"core/context.cpp",
+			"alc/context.cpp",
+			"core/converter.cpp",
+			"alc/effects/convolution.cpp",
+			"al/effects/convolution.cpp",
+			"core/cpu_caps.cpp",
+			"alc/effects/dedicated.cpp",
+			"al/effects/dedicated.cpp",
+			"core/devformat.cpp",
+			"core/device.cpp",
+			"alc/device.cpp",
+			"alc/effects/distortion.cpp",
+			"al/effects/distortion.cpp",
+			"alc/backends/dsound.cpp",
+			"alc/effects/echo.cpp",
+			"al/effects/echo.cpp",
+			"al/effect.cpp",
+			"al/effects/effects.cpp",
+			"core/effectslot.cpp",
+			"alc/effects/equalizer.cpp",
+			"al/effects/equalizer.cpp",
+			"al/error.cpp",
+			"al/event.cpp",
+			"core/except.cpp",
+			"al/eax/exception.cpp",
+			"al/extension.cpp",
+			"al/filter.cpp",
+			"core/fmt_traits.cpp",
+			"core/fpu_ctrl.cpp",
+			"al/effects/fshifter.cpp",
+			"alc/effects/fshifter.cpp",
+			"al/eax/fx_slot_index.cpp",
+			"al/eax/fx_slots.cpp",
+			"al/eax/globals.cpp",
+			"core/helpers.cpp",
+			"core/hrtf.cpp",
+			"al/listener.cpp",
+			"core/logging.cpp",
+			"alc/backends/loopback.cpp",
+			"core/mastering.cpp",
+			"core/mixer.cpp",
+			"core/mixer/mixer_c.cpp",
+			"core/mixer/mixer_sse.cpp",
+			"core/mixer/mixer_sse2.cpp",
+			"core/mixer/mixer_sse3.cpp",
+			"core/mixer/mixer_sse41.cpp",
+			"al/effects/modulator.cpp",
+			"alc/effects/modulator.cpp",
+			"core/filters/nfc.cpp",
+			"al/effects/null.cpp",
+			"alc/effects/null.cpp",
+			"alc/backends/null.cpp",
+			"alc/panning.cpp",
+			"alc/effects/pshifter.cpp",
+			"al/effects/pshifter.cpp",
+			"alc/effects/reverb.cpp",
+			"al/effects/reverb.cpp",
+			"al/source.cpp",
+			"core/filters/splitter.cpp",
+			"al/state.cpp",
+			"core/uhjfilter.cpp",
+			"core/uiddefs.cpp",
+			"al/eax/utils.cpp",
+			"alc/effects/vmorpher.cpp",
+			"al/effects/vmorpher.cpp",
+			"core/voice.cpp",
+			"alc/backends/wasapi.cpp",
+			"alc/backends/wave.cpp",
+			"alc/backends/winmm.cpp"
 		}
 
 	filter "system:linux"
